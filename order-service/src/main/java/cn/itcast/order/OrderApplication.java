4850e4003af035294c32aae7d7b35b9a14b2e400
liqingzhan <2094667478@qq.com>
2023-11-21 22:58:37 +0800

    微服务-Eureka和Nacos

---
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -0,0 +1,34 @@
+package cn.itcast.order;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@MapperScan("cn.itcast.order.mapper")
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    /**
+     * 通过定义IRule实现可以修改负载均衡规则，这种是作用于全局的
+     * @return
+     */
+    /*@Bean
+    public IRule randomRule(){
+        return new RandomRule();
+    }*/
+}
